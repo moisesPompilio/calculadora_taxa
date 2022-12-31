3452c87878c19c3b0fe674d332025c7b1709c169
moisesPompilio <moisesalexandrep.c@gmai.com>
2022-12-30 21:18:15 -0300

    integracao do sistema com o grafana e o prometheus

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 	amqp "github.com/rabbitmq/amqp091-go"
 
+	"github.com/moisesPompilio/calculadora_taxa/internal/order/infra/database"
+	"github.com/moisesPompilio/calculadora_taxa/internal/order/usecase"
 	"github.com/moisesPompilio/calculadora_taxa/pkg/rabbitmq"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil {
+		panic(err)
+	}
+
 	ch, err := rabbitmq.OpenChanel()
 	if err != nil {
 		panic(err)
 	}
-	defer ch.Close()
+	defer db.Close()
+
+	repository := database.NewOrderRepository(db)
+
+	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: repository}
 
 	out := make(chan amqp.Delivery)
 	go rabbitmq.Consume(ch, out)
+
 	for msg := range out {
-		println(string(msg.Body))
+		var inputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &inputDTO)
+		if err != nil {
+			panic(err)
+		}
+		outputDTO, err := uc.Execute(inputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		msg.Ack(false)
+		fmt.Println(outputDTO)
+		time.Sleep(500 * time.Millisecond)
 	}
 }
---
+++ cmd/producer/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/moisesPompilio/calculadora_taxa/internal/order/entity"
+)
+
+func Publish(ch *amqp.Channel, order entity.Order) error {
+	body, err := json.Marshal(order)
+	if err != nil {
+		return err
+	}
+	err = ch.Publish(
+		"amq.direct",
+		"",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        body,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GenerateOrder() entity.Order {
+	return entity.Order{
+		Id:    uuid.New().String(),
+		Price: rand.Float64() * 100,
+		Tax:   rand.Float64() * 10,
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	for i := 0; i < 100; i++ {
+		Publish(ch, GenerateOrder())
+		time.Sleep(300 * time.Millisecond)
+	}
+}
